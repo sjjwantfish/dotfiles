bb51e9caba858d5f162be22a1d11c44ce7f7df3b
sjjwantfish <sjjwantfish@163.com>
2023-03-14 07:33:14 +0800

    add sidecar polybar

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -3,6 +3,12 @@
 killall polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
+primaryMonitor=`xrandr --query | grep " connected" | grep -i "primary" | awk '{print$1}'`
+
 for m in $(polybar --list-monitors | cut -d":" -f1); do
-    MONITOR=$m polybar --reload main -c $HOME/.config/polybar/config.ini &
+  if [[ $m != $primaryMonitor ]]; then
+    MONITOR=$m polybar --reload sidecar -c $HOME/.config/polybar/config.ini &
+  else
+    PRIMARY_MONITOR=$m polybar --reload main -c $HOME/.config/polybar/config.ini &
+  fi
 done
