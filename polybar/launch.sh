1a219ada3220878b9a5813f25325f2311c4e0279
sjjwantfish <sjjwantfish@163.com>
2023-03-14 08:30:41 +0800

    fix polybar; add copyq startup

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -3,12 +3,10 @@
 killall polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-primaryMonitor=`xrandr --query | grep " connected" | grep -i "primary" | awk '{print$1}'`
+# primaryMonitor=`xrandr --query | grep " connected" | grep -i "primary" | awk '{print$1}'`
 
-for m in $(polybar --list-monitors | cut -d":" -f1); do
-  if [[ $m != $primaryMonitor ]]; then
-    MONITOR=$m polybar --reload sidecar -c $HOME/.config/polybar/config.ini &
-  else
-    PRIMARY_MONITOR=$m polybar --reload main -c $HOME/.config/polybar/config.ini &
-  fi
-done
+# for m in $(polybar --list-monitors | cut -d":" -f1); do
+#     MONITOR=$m polybar --reload main -c $HOME/.config/polybar/config.ini &
+# done
+MONITOR=DP-0 polybar --reload main -c $HOME/.config/polybar/config.ini &
+MONITOR=HDMI-0 polybar --reload sidecar -c $HOME/.config/polybar/config.ini &
