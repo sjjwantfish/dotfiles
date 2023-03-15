4fcc2c6985a3c815b36862005ffc3f3dc2690c9f
sjjwantfish <sjjwantfish@163.com>
2023-03-15 16:04:01 +0800

    remove wsl yank

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -13,24 +13,24 @@ autocmd FileType sxhkd set shiftwidth=2
 autocmd FileType yuck set shiftwidth=2
 ]])
 
--- for wsl
-vim.cmd([[
-if system('uname -r') =~ "Microsoft"
-    augroup Yank
-        autocmd!
-        autocmd TextYankPost * :call system('/mnt/c/windows/system32/clip.exe ',@")
-        augroup END
-endif
-]])
--- for wsl2
-vim.cmd([[
-if system('uname -r') =~ "microsoft"
-  augroup Yank
-  autocmd!
-  autocmd TextYankPost * :call system('/mnt/c/windows/system32/clip.exe ',@")
-  augroup END
-endif
-]])
+-- -- for wsl
+-- vim.cmd([[
+-- if system('uname -r') =~ "Microsoft"
+--     augroup Yank
+--         autocmd!
+--         autocmd TextYankPost * :call system('/mnt/c/windows/system32/clip.exe ',@")
+--         augroup END
+-- endif
+-- ]])
+-- -- for wsl2
+-- vim.cmd([[
+-- if system('uname -r') =~ "microsoft"
+--   augroup Yank
+--   autocmd!
+--   autocmd TextYankPost * :call system('/mnt/c/windows/system32/clip.exe ',@")
+--   augroup END
+-- endif
+-- ]])
 
 -- auto reload sxhkd
 vim.cmd([[
