5e42b459ab453dbe08788c0dea11c5d521dc4af0
sjjwantfish <sjjwantfish@163.com>
2023-05-08 10:54:19 +0800

    fix error config

--- nvim/lua/plugins/others.lua
+++ nvim/lua/plugins/others.lua
@@ -51,49 +51,49 @@ return {
       --   return vim.api.nvim_win_get_cursor(0)[2] > 1 and '<esc>l' or '<esc>'
       -- end,
     },
-    {
-      "chrisgrieser/nvim-early-retirement",
-      -- config = true,
-      event = "VeryLazy",
-      -- default values
-      opts = {
-        -- if a buffer has been inactive for this many minutes, close it
-        retirementAgeMins = 2,
+  },
+  {
+    "chrisgrieser/nvim-early-retirement",
+    config = true,
+    event = "VeryLazy",
+    -- default values
+    opts = {
+      -- if a buffer has been inactive for this many minutes, close it
+      retirementAgeMins = 2,
 
-        -- filetypes to ignore
-        ignoredFiletypes = {},
+      -- filetypes to ignore
+      ignoredFiletypes = {},
 
-        -- ignore files matches this lua pattern (string.find); empty string
-        -- disables this setting
-        ignoreFilenamePattern = "",
+      -- ignore files matches this lua pattern (string.find); empty string
+      -- disables this setting
+      ignoreFilenamePattern = "",
 
-        -- will not close the alternate file
-        ignoreAltFile = true,
+      -- will not close the alternate file
+      ignoreAltFile = true,
 
-        -- minimum number of open buffers for auto-closing to become active, e.g.,
-        -- by setting this to 4, no auto-closing will take place when you have 3
-        -- or less open buffers. Note that this plugin never closes the currently
-        -- active buffer, so a number < 2 will effectively disable this setting.
-        minimumBufferNum = 3,
+      -- minimum number of open buffers for auto-closing to become active, e.g.,
+      -- by setting this to 4, no auto-closing will take place when you have 3
+      -- or less open buffers. Note that this plugin never closes the currently
+      -- active buffer, so a number < 2 will effectively disable this setting.
+      minimumBufferNum = 3,
 
-        -- will ignore buffers with unsaved changes. If false, the buffers will
-        -- automatically be written and then closed.
-        ignoreUnsavedChangesBufs = true,
+      -- will ignore buffers with unsaved changes. If false, the buffers will
+      -- automatically be written and then closed.
+      ignoreUnsavedChangesBufs = true,
 
-        -- ignore non-empty buftypes, for example terminal buffers
-        ignoreSpecialBuftypes = true,
+      -- ignore non-empty buftypes, for example terminal buffers
+      ignoreSpecialBuftypes = true,
 
-        -- ignore visible buffers ("a" in `:buffers`). buffers open in a window,
-        -- or in a tab are consider visible by vim
-        ignoreVisibleBufs = true,
+      -- ignore visible buffers ("a" in `:buffers`). buffers open in a window,
+      -- or in a tab are consider visible by vim
+      ignoreVisibleBufs = true,
 
-        -- ignore unloaded buffers. session-management plugin often add buffers
-        -- to the buffer list without loading them
-        ignoreUnloadedBufs = false,
+      -- ignore unloaded buffers. session-management plugin often add buffers
+      -- to the buffer list without loading them
+      ignoreUnloadedBufs = false,
 
-        -- uses vim.notify for plugins like nvim-notify
-        notificationOnAutoClose = false,
-      },
+      -- uses vim.notify for plugins like nvim-notify
+      notificationOnAutoClose = false,
     },
   },
   {
