d5f03ca29f6aa76bb2767250f558782800760ba0
sjjwantfish <sjjwantfish@163.com>
2023-06-13 13:27:44 +0800

    nvim: change diffview file history layout

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -37,27 +37,19 @@ return {
       { "<leader>do", "<cmd>DiffviewOpen<cr>", desc = "DiffviewOpen" },
       { "<leader>dc", "<cmd>DiffviewClose<cr>", desc = "DiffviewClose" },
       { "<leader>dr", "<cmd>DiffviewRefresh<cr>", desc = "DiffviewRefresh" },
-      -- {
-      --   "<leader>db",
-      --   function()
-      --     require("telescope.builtin.git_branches")
-      --   end,
-      --   desc = "Diff with current branch",
-      -- },
     },
-    -- init = function()
-    --   require("diffview").setup()
-    -- end,
-    opts = {
-      view = {
-        merge_tool = {
-          -- layout = "diff1_plain",
-          layout = "diff3_vertical",
+    config = function()
+      local opts = {
+        view = {
+          merge_tool = {
+            layout = "diff3_vertical",
+          },
         },
-        -- file_history = {
-        --   layout = "diff2_vertical",
-        -- },
-      },
-    },
+      }
+      if vim.fn.winwidth(0) <= 120 then
+        opts.view.file_history = { layout = "diff2_vertical" }
+      end
+      require("diffview").setup(opts)
+    end,
   },
 }
