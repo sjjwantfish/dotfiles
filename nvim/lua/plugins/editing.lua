56ffb6ded68137cab8b4f17f2bf6ef11feeff9fe
sjjwantfish <sjjwantfish@163.com>
2023-06-14 16:40:01 +0800

    nvim: update plugins; add markdown preview

--- nvim/lua/plugins/editing.lua
+++ nvim/lua/plugins/editing.lua
@@ -10,4 +10,38 @@ return {
     end,
   },
   { "martinda/Jenkinsfile-vim-syntax" },
+  {
+    "numToStr/Comment.nvim",
+    -- enabled = false,
+    init = function()
+      vim.api.nvim_set_keymap(
+        "n",
+        "<leader>m",
+        "<Plug>(comment_toggle_linewise_current)",
+        -- "Comment toggle current line",
+        { noremap = true, silent = true }
+      )
+      vim.api.nvim_set_keymap(
+        "v",
+        "<leader>m",
+        "<Plug>(comment_toggle_linewise_visual)",
+        -- "Comment toggle linewise (visual)",
+        { noremap = true, silent = true }
+      )
+    end,
+    opts = {
+      ---Add a space b/w comment and the line
+      padding = true,
+      ---Whether the cursor should stay at its position
+      sticky = true,
+      ---Lines to be ignored while (un)comment
+      ignore = "^$",
+    },
+  },
+  {
+    "iamcco/markdown-preview.nvim",
+    build = function()
+      vim.fn["mkdp#util#install"]()
+    end,
+  },
 }
