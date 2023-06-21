2c7516ba0a3249feb76b73079f0a8571e8256dde
sjjwantfish <sjjwantfish@163.com>
2023-06-21 18:14:44 +0800

    nvim: add some telescope extensions

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -8,32 +8,49 @@ return {
   {
     "nvim-telescope/telescope.nvim",
     cmd = "Telescope",
-    version = false, -- telescope did only one release, so use HEAD for now
+    version = false,
     dependencies = {
       "nvim-lua/plenary.nvim",
       "debugloop/telescope-undo.nvim",
       "paopaol/telescope-git-diffs.nvim",
       "xiyaowong/telescope-emoji.nvim",
+      "nvim-telescope/telescope-live-grep-args.nvim",
+      "crispgm/telescope-heading.nvim",
+      "LinArcX/telescope-env.nvim",
     },
     config = function(_, opts)
       telescope.load_extension("vim_bookmarks")
       telescope.load_extension("undo")
       telescope.load_extension("git_diffs")
       telescope.load_extension("emoji")
+      telescope.load_extension("heading")
+      telescope.load_extension("env")
       -- telescope.load_extension("aerial")
       if vim.fn.winwidth(0) <= 120 then
         opts.defaults = vim.tbl_deep_extend("force", opts.defaults, require("telescope.themes").get_dropdown({ opts }))
-        -- opts.defaults = require("telescope.themes").get_dropdown({ opts })
-        -- telescope.setup({ defaults = require("telescope.themes").get_dropdown({ opts }) })
       end
+      local lga_actions = require("telescope-live-grep-args.actions")
+      opts.extensions = {
+        undo = {},
+        live_grep_args = {
+          auto_quoting = true,
+          mappings = {
+            i = {
+              -- ["<C-k>"] = lga_actions.quote_prompt(),
+              ["<C-i>"] = lga_actions.quote_prompt({ postfix = " --iglob " }),
+            },
+          },
+        },
+        heading = { treesitter = true },
+      }
       telescope.setup(opts)
     end,
     keys = {
-      -- add a keymap to browse plugin files
-      -- stylua: ignore
       {
         "<leader>fp",
-        function() require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root }) end,
+        function()
+          require("telescope.builtin").find_files({ cwd = require("lazy.core.config").options.root })
+        end,
         desc = "Find Plugin File",
       },
       {
@@ -58,9 +75,12 @@ return {
       },
       { "<leader>ca", false },
       { "<leader>/", false },
-      -- { "gr", false },
-      -- { "gd", false },
-      { "<leader>p", "<cmd>Telescope live_grep<cr>", desc = "Find in files(Grep)" },
+      -- { "<leader>p", "<cmd>Telescope live_grep<cr>", desc = "Find in files(Grep)" },
+      {
+        "<leader>p",
+        ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>",
+        desc = "Live Grep with args",
+      },
     },
     opts = {
       pickers = {
@@ -85,13 +105,6 @@ return {
             "!vendor",
           },
         },
-        -- Default configuration for builtin pickers goes here:
-        -- picker_name = {
-        --   picker_config_key = value,
-        --   ...
-        -- }
-        -- Now the picker_config_key will be applied every time you call this
-        -- builtin picker
       },
       defaults = {
         mappings = {
@@ -102,16 +115,16 @@ return {
               return require("trouble.providers.telescope").open_with_trouble(...)
             end,
             ["<C-Down>"] = function(...)
-              return require("telescope.actions").cycle_history_next(...)
+              return actions.cycle_history_next(...)
             end,
             ["<C-Up>"] = function(...)
-              return require("telescope.actions").cycle_history_prev(...)
+              return actions.cycle_history_prev(...)
             end,
             ["<C-f>"] = function(...)
-              return require("telescope.actions").preview_scrolling_down(...)
+              return actions.preview_scrolling_down(...)
             end,
             ["<C-b>"] = function(...)
-              return require("telescope.actions").preview_scrolling_up(...)
+              return actions.preview_scrolling_up(...)
             end,
           },
           n = {
@@ -122,19 +135,6 @@ return {
             ["k"] = actions.move_selection_previous,
           },
         },
-      },
-      extensions = {
-        undo = {
-          -- telescope-undo.nvim config, see below
-        },
-        -- aerial = {
-        --   -- Display symbols as <root>.<parent>.<symbol>
-        --   show_nesting = {
-        --     ["_"] = false, -- This key will be the default
-        --     json = true, -- You can set the option for specific filetypes
-        --     yaml = true,
-        --   },
-        -- },
       },
     },
   },
