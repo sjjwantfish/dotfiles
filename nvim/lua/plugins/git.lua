ead76661f1fcae229e63e49eb035acdbe7f4dee8
sjjwantfish <sjjwantfish@163.com>
2023-06-21 18:15:12 +0800

    nvim: change git keymap

--- nvim/lua/plugins/git.lua
+++ nvim/lua/plugins/git.lua
@@ -14,16 +14,16 @@ return {
       -- stylua: ignore start
       map("n", "<leader>gj", gs.next_hunk, "Next Hunk")
       map("n", "<leader>gk", gs.prev_hunk, "Prev Hunk")
-      map({ "n", "v" }, "<leader>gs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
+      -- map({ "n", "v" }, "<leader>gs", ":Gitsigns stage_hunk<CR>", "Stage Hunk")
       map({ "n", "v" }, "<leader>gr", ":Gitsigns reset_hunk<CR>", "Reset Hunk")
-      map("n", "<leader>gS", gs.stage_buffer, "Stage Buffer")
-      map("n", "<leader>gu", gs.undo_stage_hunk, "Undo Stage Hunk")
+      -- map("n", "<leader>gS", gs.stage_buffer, "Stage Buffer")
+      -- map("n", "<leader>gu", gs.undo_stage_hunk, "Undo Stage Hunk")
       map("n", "<leader>gR", gs.reset_buffer, "Reset Buffer")
       map("n", "<leader>gp", gs.preview_hunk, "Preview Hunk")
       map("n", "<leader>gb", function() gs.blame_line({ full = true }) end, "Blame Line")
       map("n", "<leader>gd", gs.diffthis, "Diff This")
       map("n", "<leader>gD", function() gs.diffthis("~") end, "Diff This ~")
-      map("n", "<leader>gf", "<cmd>Telescope git_status<cr>", "Git status")
+      -- map("n", "<leader>gf", "<cmd>Telescope git_status<cr>", "Git status")
       map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>", "GitSigns Select Hunk")
       end,
     },
