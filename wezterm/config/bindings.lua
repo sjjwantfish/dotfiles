8b194622a5b90b80ba3cf94c59a099f9dfcdcdce
sjjwantfish <sjjwantfish@163.com>
2023-11-17 15:02:22 +0800

    wezterm: copy new config

---
+++ wezterm/config/bindings.lua
@@ -0,0 +1,143 @@
+local wezterm = require("wezterm")
+local platform = require("utils.platform")()
+local act = wezterm.action
+
+local mod = {}
+
+if platform.is_mac then
+	-- mod.SUPER = 'SUPER'
+	-- mod.SUPER_REV = 'SUPER|CTRL'
+	mod.SUPER = "ALT"
+	mod.SUPER_REV = "ALT|CTRL"
+elseif platform.is_win then
+	mod.SUPER = "ALT" -- to not conflict with Windows key shortcuts
+	mod.SUPER_REV = "ALT|CTRL"
+end
+
+local keys = {
+	{ key = "z", mods = mod.SUPER_REV, action = act.TogglePaneZoomState },
+	-- paste
+	{ key = "v", mods = mod.SUPER, action = act({ PasteFrom = "Clipboard" }) },
+	{
+		key = "s",
+		mods = mod.SUPER,
+		action = wezterm.action.QuickSelectArgs({
+			patterns = {
+				"[\\w/:.,'\"%%%-@_~]+",
+			},
+		}),
+	},
+	{
+		key = ";",
+		mods = mod.SUPER,
+		action = wezterm.action.QuickSelectArgs({
+			label = "open url",
+			patterns = {
+				"https?://\\S+",
+			},
+			action = wezterm.action_callback(function(window, pane)
+				local url = window:get_selection_text_for_pane(pane)
+				wezterm.log_info("opening: " .. url)
+				wezterm.open_with(url)
+			end),
+		}),
+	},
+	-- copy mode
+	{
+		key = "c",
+		mods = mod.SUPER,
+		action = act.Multiple({
+			act.CopyMode("ClearSelectionMode"),
+			act.ActivateCopyMode,
+			act.ClearSelection,
+		}),
+	},
+	-- search
+	{
+		key = "/",
+		mods = mod.SUPER,
+		action = act.Multiple({ act.ClearSelection, act.Search({ CaseInSensitiveString = "" }) }),
+	},
+	{
+		key = "f",
+		mods = mod.SUPER,
+		action = act.Multiple({ act.ClearSelection, act.Search({ CaseInSensitiveString = "" }) }),
+	},
+	{
+		key = "F",
+		mods = mod.SUPER,
+		action = act.Multiple({ act.ClearSelection, act.Search({ CaseSensitiveString = "" }) }),
+	},
+	-- close pane
+	{ key = "w", mods = mod.SUPER, action = act({ CloseCurrentPane = { confirm = true } }) },
+	-- Activate Pane
+	{ key = "t", mods = mod.SUPER, action = act({ SpawnTab = "CurrentPaneDomain" }) },
+	{
+		key = "-",
+		mods = mod.SUPER,
+		action = act({ SplitVertical = { domain = "CurrentPaneDomain" } }),
+	},
+	{
+		key = "\\",
+		mods = mod.SUPER,
+		action = act({ SplitHorizontal = { domain = "CurrentPaneDomain" } }),
+	},
+	{ key = "LeftArrow", mods = mod.SUPER, action = act({ ActivatePaneDirection = "Left" }) },
+	{ key = "RightArrow", mods = mod.SUPER, action = act({ ActivatePaneDirection = "Right" }) },
+	{ key = "UpArrow", mods = mod.SUPER, action = act({ ActivatePaneDirection = "Up" }) },
+	{ key = "DownArrow", mods = mod.SUPER, action = act({ ActivatePaneDirection = "Down" }) },
+	-- Activate Tab
+	{ key = "1", mods = mod.SUPER, action = act({ ActivateTab = 0 }) },
+	{ key = "2", mods = mod.SUPER, action = act({ ActivateTab = 1 }) },
+	{ key = "3", mods = mod.SUPER, action = act({ ActivateTab = 2 }) },
+	{ key = "4", mods = mod.SUPER, action = act({ ActivateTab = 3 }) },
+	{ key = "5", mods = mod.SUPER, action = act({ ActivateTab = 4 }) },
+	{ key = "6", mods = mod.SUPER, action = act({ ActivateTab = 5 }) },
+	{ key = "7", mods = mod.SUPER, action = act({ ActivateTab = 6 }) },
+	{ key = "8", mods = mod.SUPER, action = act({ ActivateTab = 7 }) },
+	{ key = "9", mods = mod.SUPER, action = act({ ActivateTab = 8 }) },
+	-- launcher
+	{ key = "r", mods = mod.SUPER, action = wezterm.action.ShowLauncher },
+	{ key = "p", mods = mod.SUPER, action = wezterm.action.ActivateCommandPalette },
+	{
+		key = "`",
+		mods = mod.SUPER,
+		action = act.RotatePanes("CounterClockwise"),
+	},
+	-- scroll
+	{ key = "k", mods = mod.SUPER, action = act({ ScrollByPage = -1 }) },
+	{ key = "j", mods = mod.SUPER, action = act({ ScrollByPage = 1 }) },
+	-- rename tab
+	{
+		key = "R",
+		mods = "ALT|SHIFT",
+		action = act.PromptInputLine({
+			description = "Enter new name for tab",
+			action = wezterm.action_callback(function(window, pane, line)
+				if line then
+					window:active_tab():set_title(line)
+				end
+			end),
+		}),
+	},
+}
+local mouse_bindings = {
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "CTRL",
+		action = "OpenLinkAtMouseCursor",
+	},
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "SHIFT",
+		action = wezterm.action.ExtendSelectionToMouseCursor("Word"),
+	},
+}
+
+return {
+	disable_default_key_bindings = true,
+	-- disable_default_mouse_bindings = true,
+	-- leader = { key = 'Space', mods = 'ALT|SHIFT' },
+	keys = keys,
+	mouse_bindings = mouse_bindings,
+}
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,126 +1,13 @@
-local wezterm = require("wezterm")
-local act = wezterm.action
--- local wsl_domains = wezterm.default_wsl_domains()
--- for _, dom in ipairs(wsl_domains) do
--- 	dom.default_cwd = "~"
--- 	dom.username = "sujiajun"
--- 	-- dom.default_prog = { "zsh" }
--- end
+local Config = require('config')
 
-local default_prog = nil
-local launch_menu = nil
-if wezterm.target_triple == "x86_64-pc-windows-msvc" then
-	wezterm.log_info("load windows config")
-	local pwsh_prog = { os.getenv("LOCALAPPDATA") .. "\\Microsoft\\WindowsApps\\pwsh.exe" }
-	local wsl_arch_prog = { "wsl.exe", "-d", "Arch20230315", "-u", "sujiajun", "--cd", "~" }
+-- require('events.right-status').setup()
+require('events.tab-title').setup()
+require('events.new-tab-button').setup()
 
-	default_prog = pwsh_prog
-	launch_menu = {
-		{
-			label = "Arch20230315",
-			args = wsl_arch_prog,
-			domain = { DomainName = "wsl" },
-		},
-		{
-			label = "pwsh",
-			args = pwsh_prog,
-			domain = { DomainName = "local" },
-		},
-	}
-end
-
-local search_pattern = {}
-
-local config = {
-	color_scheme = "Andromeda",
-	window_background_opacity = 0.9,
-	disable_default_key_bindings = false,
-	default_prog = default_prog,
-	launch_menu = launch_menu,
-	font_size = 16,
-	keys = {
-		-- paste
-		{ key = "v", mods = "ALT", action = act({ PasteFrom = "Clipboard" }) },
-		{ key = "v", mods = "WIN", action = act({ PasteFrom = "Clipboard" }) },
-		-- quick select mode
-		-- { key = "s", mods = "ALT", action = wezterm.action.QuickSelect },
-		{
-			key = "s",
-			mods = "ALT",
-			action = wezterm.action.QuickSelectArgs({
-				patterns = {
-					-- "\\w+",
-					"[\\w/:.,'\"%@]+",
-				},
-			}),
-		},
-		{
-			key = "l",
-			mods = "ALT",
-			action = wezterm.action.QuickSelectArgs({
-				label = "open url",
-				patterns = {
-					"https?://\\S+",
-				},
-				action = wezterm.action_callback(function(window, pane)
-					local url = window:get_selection_text_for_pane(pane)
-					wezterm.log_info("opening: " .. url)
-					wezterm.open_with(url)
-				end),
-			}),
-		},
-		-- copy mode
-		{
-			key = "c",
-			mods = "ALT",
-			action = act.Multiple({ act.CopyMode("ClearSelectionMode"), act.ActivateCopyMode, act.ClearSelection }),
-		},
-		-- search
-		{
-			key = "/",
-			mods = "ALT",
-			action = act.Multiple({ act.ClearSelection, act.Search({ CaseSensitiveString = "" }) }),
-		},
-		-- close pane
-		{ key = "w", mods = "ALT", action = act({ CloseCurrentPane = { confirm = true } }) },
-		-- Activate Pane
-		{ key = "t", mods = "SUPER", action = act({ SpawnTab = "CurrentPaneDomain" }) },
-		{ key = "t", mods = "ALT", action = act({ SpawnTab = "CurrentPaneDomain" }) },
-		{ key = "-", mods = "ALT", action = act({ SplitVertical = { domain = "CurrentPaneDomain" } }) },
-		{ key = "\\", mods = "ALT", action = act({ SplitHorizontal = { domain = "CurrentPaneDomain" } }) },
-		{ key = "LeftArrow", mods = "ALT", action = act({ ActivatePaneDirection = "Left" }) },
-		{ key = "RightArrow", mods = "ALT", action = act({ ActivatePaneDirection = "Right" }) },
-		{ key = "UpArrow", mods = "ALT", action = act({ ActivatePaneDirection = "Up" }) },
-		{ key = "DownArrow", mods = "ALT", action = act({ ActivatePaneDirection = "Down" }) },
-		-- Activate Tab
-		{ key = "1", mods = "ALT", action = act({ ActivateTab = 0 }) },
-		{ key = "2", mods = "ALT", action = act({ ActivateTab = 1 }) },
-		{ key = "3", mods = "ALT", action = act({ ActivateTab = 2 }) },
-		{ key = "4", mods = "ALT", action = act({ ActivateTab = 3 }) },
-		{ key = "5", mods = "ALT", action = act({ ActivateTab = 4 }) },
-		{ key = "6", mods = "ALT", action = act({ ActivateTab = 5 }) },
-		{ key = "7", mods = "ALT", action = act({ ActivateTab = 6 }) },
-		{ key = "8", mods = "ALT", action = act({ ActivateTab = 7 }) },
-		{ key = "9", mods = "ALT", action = act({ ActivateTab = 8 }) },
-		-- launcher
-		{ key = "r", mods = "ALT", action = wezterm.action.ShowLauncher },
-		{ key = "p", mods = "ALT", action = wezterm.action.ActivateCommandPalette },
-		{
-			key = "`",
-			mods = "ALT",
-			action = act.RotatePanes("CounterClockwise"),
-		},
-		-- scroll
-		{ key = "b", mods = "ALT", action = act({ ScrollByPage = -1 }) },
-		{ key = "f", mods = "ALT", action = act({ ScrollByPage = 1 }) },
-	},
-	mouse_bindings = {
-		{
-			event = { Up = { streak = 1, button = "Left" } },
-			mods = "CTRL",
-			action = "OpenLinkAtMouseCursor",
-		},
-	},
-}
-
-return config
+return Config:init()
+  :append(require('config.appearance'))
+  :append(require('config.bindings'))
+  :append(require('config.domains'))
+  :append(require('config.fonts'))
+  :append(require('config.general'))
+  :append(require('config.launch')).options
