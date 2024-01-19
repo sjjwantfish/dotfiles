18ef2b3a8f85bd692db4a5737a8fc53bbbbeb116
sjjwantfish <sjjwantfish@163.com>
2024-01-19 18:59:20 +0800

    add new config example

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,3 +1,15 @@
+-- local wezterm = require("wezterm")
+-- config = {
+-- 	font_size = 16,
+-- 	color_scheme = "Catppuccin Mocha",
+-- 	use_fancy_tab_bar = false,
+-- 	show_new_tab_button_in_tab_bar = false,
+-- 	window_background_opacity = 0.9,
+-- 	-- macos_window_background_blur = 70,
+-- 	adjust_window_size_when_changing_font_size = false,
+-- }
+
+-- return config
 local Config = require("config")
 
 require("events.right-status").setup()
