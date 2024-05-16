c45142ade24762692a79ec6a363cf6df6fdc47fc
sjjwantfish <sjjwantfish@163.com>
2024-05-16 10:33:29 +0800

    skhd: fix swap command

--- skhd/yabai/swap.sh
+++ skhd/yabai/swap.sh
@@ -3,7 +3,7 @@ dir=$1
 yabai -m window --swap $dir
 [[ $? -eq 0 ]] && exit 0
 
-display=$(yabai -m query --displays --display $dir | jq '.id')
+display=$(yabai -m query --displays --display $dir | jq '.index')
 [[ ! $display ]] && exit -1
 
 dirWardSpace=$(yabai -m query --spaces | jq ".[] | select((.display==$display) and (.[\"is-visible\"]==true)) | .index")
